a63ed6b38fc7cb5e76b13b12bcf7b0704ab08daa
Georgi Ivanov <georgiivanov@Georgis-Mac.local>
2013-10-27 17:05:00 +0200

    test

---
+++ haha/haha/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  haha
+//
+//  Created by Georgi Ivanov on 10/27/13.
+//  Copyright (c) 2013 Georgi Ivanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ haha/haha/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  haha
+//
+//  Created by Georgi Ivanov on 10/27/13.
+//  Copyright (c) 2013 Georgi Ivanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
