bbccb9033735f20118e882bfc89aa437669aa1e4
Georgi Ivanov <georgiivanov@Georgis-Mac.local>
2013-10-27 11:47:08 +0200

    wired some controls and actions

--- HelloWorld_1/ViewController.m
+++ HelloWorld_1/ViewController.m
@@ -14,16 +14,21 @@
 
 @implementation ViewController
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.messageLabel.text = @"Hello iOS";
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
+    
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)buttonClicked:(id)sender {
+}
 @end
